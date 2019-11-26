bed5269ea31d5e5db7b5b507dd333c59565dd7b4
riz1702 <rajshree.dehulia@gmail.com>
2019-11-26 13:50:51 +0000

    test passing

---
+++ lib/bookmark.rb
@@ -0,0 +1,8 @@
+class Bookmark
+
+  def self.all
+    bookmarks = [ "https://www.funology.com", "https://www.switchzoo.com", "http://bubbles.org" , "https://www.funbrain.com" ]
+  end
+
+
+end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -5,10 +5,10 @@ describe 'Bookmark' do
   describe '.all' do
     it 'returns all the bookmarks saved' do
       bookmarks = Bookmark.all
-      expect(page).to include ("https://www.funology.com")
-      expect(page).to include ("https://www.switchzoo.com")
-      expect(page).to include ("http://bubbles.org")
-      expect(page).to include ("https://www.funbrain.com")
+      expect(bookmarks).to include ("https://www.funology.com")
+      expect(bookmarks).to include ("https://www.switchzoo.com")
+      expect(bookmarks).to include ("http://bubbles.org")
+      expect(bookmarks).to include ("https://www.funbrain.com")
 
     end
   end
