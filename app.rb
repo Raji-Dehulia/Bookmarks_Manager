c037efa4fb750620a41d0d717d840d112725eea4
riz1702 <rajshree.dehulia@gmail.com>
2019-11-26 11:37:09 +0000

    viewing bookmarks test passed

--- app.rb
+++ app.rb
@@ -2,9 +2,16 @@ require 'sinatra/base'
 
 class BookmarkManager < Sinatra::Base
 
-  get '/' do
-    "Bookmark Manager"
-  end
+  get '/bookmarks' do
+    bookmarks = ["https://github.com/makersacademy/course/blob/master/bookmark_manager/03_viewing_bookmarks.md",
+      "https://github.com/makersacademy/course/blob/master/apprenticeship_module_outlines.md",
+      "https://dev.to/jeremy/ruby-method-spotlight-slice-1f3j"]
 
-  run! if app_file == $0
-end
+      bookmarks.join("   ***  ") #this will convert array items in strings separated by ***
+
+    end
+
+
+
+    run! if app_file == $0
+  end
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,7 +1,6 @@
 feature 'Viewing bookmarks' do
   scenario 'user being able to see bookmarks page' do
     visit('/bookmarks')
-    expect(page).to have_content "Bookmark Manager"
     expect(page).to have_content "https://github.com/makersacademy/course/blob/master/bookmark_manager/03_viewing_bookmarks.md"
     expect(page).to have_content "https://github.com/makersacademy/course/blob/master/apprenticeship_module_outlines.md"
     expect(page).to have_content "https://dev.to/jeremy/ruby-method-spotlight-slice-1f3j"
